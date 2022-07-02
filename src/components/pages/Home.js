2975e09b597a989144e1a4c5ed62bc836296da22
J Semidey <mtsemidey@gmail.com>
2022-07-02 18:32:43 -0400

    set up home in style of terminal clone

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,19 +1,37 @@
 import React from 'react';
 
 function Home() {
-  return (
-    <div className="home">
-      <h1>Home</h1>
-      <p>
-        This is the HOME page of the web application.
-      </p>
 
-      <div className='dosContainer border border-dark'>
-        <p className='dosBar'>
-        <i class="bi bi-terminal"></i>
-          JSEM:/c/Users/JackSemidey
-        </p>  
-      </div>
+
+
+  return (
+    <div className="container">
+
+      <div className='my-5 dosContainer border border-secondary bg-secondary text-light col-12 shadow'>
+
+        <div className='dosBar px-1 d-flex justify-content-between'>
+          <div>
+            <i class="bi bi-terminal"></i> JSEM64:/c/Users/Jack
+          </div>
+
+          <div className="px-1">
+            <span className="mx-1"><i class="bi bi-dash"></i></span>
+            <span className="mx-1"><i class="bi bi-square"></i></span>
+            <span className="mx-1"><i class="bi bi-x-square"></i></span>
+          </div>
+        </div>
+
+        <div className="dosContent bg-dark p-1">
+          <p className="text-success">Jack <span className="text-purple">JSEM64</span> 
+          <span className="text-warning"> ~/JSPortfolio</span> 
+          <span className="text-primary"> (main) </span></p>
+          <p>$ Hello World!</p>
+          <p>$ My name is Jack</p>
+          <p>$ Full Stack Web Developer</p>
+          <p>$ Would you like to know more? <span className="showMore">YES _</span></p>
+        </div>
+
+      </div> 
     </div>
   );
 };
